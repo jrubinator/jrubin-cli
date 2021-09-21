66d9ce4c4c15947a7f4f6657cda00cb7d7fe9475
jrubinator <jjrs.pam+github@gmail.com>
2021-09-21 17:08:49 -0400

    notice when your workspace is unclean

---
+++ bash-extensions/workspace.bash
@@ -0,0 +1,14 @@
+function unclean {
+    local_branches="$(gb | grep -v master)"
+    if [[ ! -z $local_branches ]]; then
+        printf "Local branches:\n$local_branches\n\n"
+    fi
+    changed_files="$(git status --porcelain=v1 2>/dev/null)"
+    if [[ ! -z $changed_files ]]; then
+        printf "Uncommitted files:\n$changed_files\n\n"
+    fi
+    open_jobs=$(jobs)
+    if [[ ! -z $open_jobs ]]; then
+        printf "Jobs:\n$open_jobs\n\n"
+    fi
+}
