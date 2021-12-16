4946eba64ec8a82a5a9495da1347426411d0d90b
jrubinator <jjrs.pam+github@gmail.com>
2021-12-16 15:30:48 -0500

    Rearchitect everything to note shells

--- shell-extensions/bash.bash
+++ shell-extensions/bash.bash
@@ -1,5 +1,5 @@
 # Source this file to source all files in bash-extensions
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-for f in $BASE_DIR/bash-extensions/*.bash; do
+for f in $BASE_DIR/bash/*.bash; do
     source $f
 done
