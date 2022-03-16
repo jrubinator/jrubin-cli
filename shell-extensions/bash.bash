7f9a5a788b074eeb15b2a0e8d4ce1f386a151bf9
jrubinator <jjrs.pam+github@gmail.com>
2022-03-16 16:02:44 -0400

    Don't source .pl files

--- shell-extensions/bash.bash
+++ shell-extensions/bash.bash
@@ -3,6 +3,6 @@ BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SCRIPT_DIR="$BASE_DIR/../scripts"
 export PATH="$SCRIPT_DIR/other:$SCRIPT_DIR/git:$SCRIPT_DIR/grep:$PATH"
 
-for f in $BASE_DIR/bash/* $BASE_DIR/shared/*; do
+for f in $BASE_DIR/bash/*.bash $BASE_DIR/shared/*; do
     source $f
 done
